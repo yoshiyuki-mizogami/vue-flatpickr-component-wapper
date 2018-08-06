fc1bc03e7bb26c9e86f747f4f800de277ed981d0
yoshiyuki.mizogami <mizogami00@gmail.com>
2018-08-06 22:33:54 +0900

    add bundle

--- src/vue/root.vue
+++ src/vue/root.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="main">
     <fl v-model="date" @change="changeHook" :config="{allowInput:true}"/>
-    <fl v-model="date2" @change="changeHook"/>
+    <fl v-model="date2" @change="changeHook" :config="{locale:ja}"/>
   </div>
 </template>
 <style>
 </style>
 <script>
 import fl from '../../fl-vue-wrapper.vue'
-import ja from 'flatpickr/dist/l10n/ja'
+import {ja} from 'flatpickr/dist/l10n/ja'
 export default {
   data(){
     return {
       date:null,
-      date2:null
+      date2:null,
+      ja
     }
   },
   created(){
-    setTimeout(()=>{
+    setTimeout(()=>{ 
       this.date = new Date()
     }, 3000)
   },
